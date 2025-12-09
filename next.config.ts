82fdd42fcff82d247b445d4ad3b2818e4e2999c9
adomtadjoudine <tadjoudine.adom@ifnti.com>
2025-12-09 14:32:52 +0000

    e

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+   typescript: {
+    // ⚠️ Ignore les erreurs TypeScript pendant le build
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Optionnel : ignorer aussi ESLint
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
